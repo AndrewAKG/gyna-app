7c743717f178460f52cf9adc3d1e8cff011fdf2e
TheDreamHunter <andrew.kamal@student.guc.edu.eg>
2017-08-09 10:39:08 +0200

    fix: loading screen phase 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
 import React from 'react';
+import { Asset } from 'expo';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import Navigator from './screens/Navigator';
 import reducers from './reducers';
+import LoadingScreen from './screens/LoadingScreen';
 
 export default class App extends React.Component {
+
+  state = {
+    Ready: false,
+    appLoaded: false
+  }
+
+  componentWillMount() {
+    this._cacheResourceAsync();
+  }
+
+  componentDidMount() {
+    this.appLoaded();
+  }
+
+  async _cacheResourceAsync() {
+    const images = [
+      require('../assets/icons/1.png'),
+      require('../assets/icons/2.png'),
+      require('../assets/icons/bigZ.png'),
+      require('../assets/icons/welcome.png'),
+      require('../assets/icons/4.png')
+    ];
+    for (let image of images) {
+      await Asset.fromModule(image).downloadAsync();
+    }
+    this.setState({ Ready: true })
+  }
+
+  appLoaded = async () => {
+    const promises = assets.map(module => Asset.fromModule(module).downloadAsync());
+    await Promise.all(promises);
+    this.setState({ appLoaded: true });
+  }
+
   render() {
+    if (!this.state.appLoaded) {
+      return <LoadingScreen />
+    }
     return (
       <Provider store={createStore(reducers, {}, applyMiddleware(ReduxThunk))}>
         <Navigator />
---
+++ src/screens/LoadingScreen.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Image, Dimensions, View } from 'react-native';
+import BackgroundImage from './BackgroundImage';
+
+const { height, width } = Dimensions.get('window');
+
+class LoadingScreen extends Component {
+  render() {
+    return (
+      <BackgroundImage>
+        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+          <Image
+            style={styles.LoadingImageStyle}
+            source={require('../../assets/icons/bigZ.png')}
+          />
+        </View>
+      </BackgroundImage>
+    );
+  }
+}
+
+const styles = {
+  LoadingImageStyle: {
+    flex: 1,
+    width: 0.5 * width,
+    height: 0.5 * height
+  }
+};
+
+export default LoadingScreen;
