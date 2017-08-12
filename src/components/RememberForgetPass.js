ecadfeae4a9540c6d575c42f0f8a71872f989abb
TheDreamHunter <andrew.kamal@student.guc.edu.eg>
2017-08-12 14:14:54 +0200

    fix: re-arranging remember me. forget password into new component

---
+++ src/components/RememberForgetPass.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Button, CheckBox } from 'react-native-elements';
+
+class RememberForgetPass extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { checked: false }
+  }
+
+  onPress() {
+    this.setState({ checked: !(this.state.checked) })
+  }
+
+  renderCheckBox() {
+    return (
+      <CheckBox
+        center
+        title='Remember Me'
+        checkedIcon='check-square-o'
+        uncheckedIcon='square-o'
+        checkedColor='white'
+        checked={this.state.checked}
+        onIconPress={() => this.onPress()}
+        containerStyle={styles.checkboxStyle}
+        textStyle={{ color: 'white', fontWeight: "normal", textAlign: "left", fontSize: 12 }}
+      />
+    );
+  }
+
+  render() {
+    const {
+      checkboxContainerStyle,
+      CheckBoxViewStyle,
+      buttonTextStyle,
+      forgetPasswordViewStyle,
+      forgerPasswordButtonStyle
+    } = styles;
+
+    return (
+      <View style={CheckBoxViewStyle}>
+
+        <View style={checkboxContainerStyle}>
+          {this.renderCheckBox()}
+        </View>
+
+        <View style={forgetPasswordViewStyle}>
+          <Button
+            onPress={() => navigate('signUp')}
+            title="forget your password?"
+            buttonStyle={forgerPasswordButtonStyle}
+            color='white'
+            fontWeight='600'
+            textStyle={buttonTextStyle}
+          />
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const styles = {
+  forgerPasswordButtonStyle: {
+    borderRadius: 0,
+    backgroundColor: 'rgba(0,0,0,0)',
+    width: 150,
+    height: 100,
+    marginTop: -14,
+  },
+  buttonTextStyle: {
+    textDecorationLine: 'underline',
+    fontSize: 12,
+    textAlign: 'left',
+  },
+  forgetPasswordViewStyle: {
+    flex: 5,
+    marginTop: -10,
+    marginRight: 20
+  },
+  CheckBoxViewStyle: {
+    flexDirection: 'row',
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0)',
+    margin: 15
+  },
+  checkboxContainerStyle: {
+    flex: 5,
+    marginLeft: 6,
+    alignItems: 'flex-start'
+  },
+  checkboxStyle: {
+    backgroundColor: 'rgba(0,0,0,0)',
+    borderRadius: 0,
+    borderWidth: 0
+  }
+}
+
+export default RememberForgetPass;
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,6 +4,7 @@ import { Button, CheckBox } from 'react-native-elements';
 import BackgroundImage from '../components/BackgroundImage';
 import { Input } from '../components/Input';
 import { InputPassword } from '../components/InputPassword';
+import RememberForgetPass from '../components/RememberForgetPass';
 
 const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height;
@@ -21,41 +22,11 @@ class LoginScreen extends Component {
     headerLeft: null
   };
 
-  constructor(props) {
-    super(props);
-    this.state = { checked: false }
-  }
-
-  renderCheckBox() {
-    return (
-      <CheckBox
-        center
-        title='Remember Me'
-        checkedIcon='check-square-o'
-        uncheckedIcon='square-o'
-        checkedColor='white'
-        checked={this.state.checked}
-        onIconPress={() => this.onPress()}
-        containerStyle={styles.checkboxStyle}
-        textStyle={{ color: 'white', fontWeight: "normal", textAlign: "left", fontSize: 12 }}
-      />
-    );
-  }
-
-  onPress() {
-    this.setState({ checked: !(this.state.checked) })
-  }
-
   render() {
     const { navigate } = this.props.navigation;
     const {
       containerStyle,
-      checkboxContainerStyle,
-      CheckBoxViewStyle,
       buttonStyle,
-      buttonTextStyle,
-      forgetPasswordViewStyle,
-      forgerPasswordButtonStyle
     } = styles;
 
     return (
@@ -87,24 +58,7 @@ class LoginScreen extends Component {
               fontSize={18}
             />
 
-            <View style={CheckBoxViewStyle}>
-
-              <View style={checkboxContainerStyle}>
-                {this.renderCheckBox()}
-              </View>
-
-              <View style={forgetPasswordViewStyle}>
-                <Button
-                  onPress={() => navigate('signUp')}
-                  title="forget your password?"
-                  buttonStyle={forgerPasswordButtonStyle}
-                  color='white'
-                  fontWeight='600'
-                  textStyle={buttonTextStyle}
-                />
-              </View>
-
-            </View>
+            <RememberForgetPass />
 
           </ScrollView>
 
@@ -126,39 +80,6 @@ const styles = {
     width: 0.8 * SCREEN_WIDTH,
     height: 60,
     margin: 10
-  },
-  forgerPasswordButtonStyle: {
-    borderRadius: 0,
-    backgroundColor: 'rgba(0,0,0,0)',
-    width: 150,
-    height: 100,
-    marginTop: -14,
-  },
-  buttonTextStyle: {
-    textDecorationLine: 'underline',
-    fontSize: 12,
-    textAlign: 'left',
-  },
-  forgetPasswordViewStyle: {
-    flex: 5,
-    marginTop: -10,
-    marginRight: 20
-  },
-  CheckBoxViewStyle: {
-    flexDirection: 'row',
-    flex: 1,
-    backgroundColor: 'rgba(0,0,0,0)',
-    margin: 15
-  },
-  checkboxContainerStyle: {
-    flex: 5,
-    marginLeft: 6,
-    alignItems: 'flex-start'
-  },
-  checkboxStyle: {
-    backgroundColor: 'rgba(0,0,0,0)',
-    borderRadius: 0,
-    borderWidth: 0
   }
 };
 
