c246c11842735df9d4823dd36c7cfd72e55c8529
SallyHabib <sally.hanen@student.guc.edu.eg>
2017-08-12 20:15:58 +0200

    fix:style

--- src/components/SignUpAccount.js
+++ src/components/SignUpAccount.js
@@ -6,71 +6,71 @@ const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 class SignUpAccount extends React.Component {
-	render() {
-		const { onButtonPress } = this.props;
-		const {
+  render() {
+    const { onButtonPress } = this.props;
+    const {
       containerStyle,
-			TextViewStyle,
-			noAccountTextStyle,
-			SignUpContainer,
-			SignUpButtonStyle,
-			buttonTextStyle
+      TextViewStyle,
+      noAccountTextStyle,
+      SignUpContainer,
+      SignUpButtonStyle,
+      buttonTextStyle
    } = styles;
 
-		return (
-			<View style={containerStyle}>
+    return (
+      <View style={containerStyle}>
 
-				<View style={TextViewStyle}>
-					<Text style={noAccountTextStyle}>
-						Don't Have an account ?
+        <View style={TextViewStyle}>
+          <Text style={noAccountTextStyle}>
+            Don't Have an account ?
           </Text>
-				</View>
+        </View>
 
-				<Button
-					onPress={onButtonPress}
-					title="Sign Up"
-					buttonStyle={SignUpButtonStyle}
-					color='white'
-					fontWeight='normal'
-					textStyle={buttonTextStyle}
-					containerViewStyle={SignUpContainer}
-				/>
-
-			</View>
-		);
-	}
+          <Button
+            onPress={onButtonPress}
+            title="Sign Up"
+            buttonStyle={SignUpButtonStyle}
+            color='white'
+            fontWeight='normal'
+            textStyle={buttonTextStyle}
+            containerViewStyle={SignUpContainer}
+          />
+          
+      </View>
+    );
+  }
 }
 
 const styles = {
-	containerStyle: {
-		flexDirection: 'row',
-		flex: 1,
-		backgroundColor: 'rgba(0,0,0,0)',
-		margin: 10
-	},
-	SignUpButtonStyle: {
-		borderRadius: 0,
-		backgroundColor: 'rgba(0,0,0,0)'
-	},
-	SignUpContainer: {
-		flex: 3,
-		alignItems: 'stretch'
-	},
-	buttonTextStyle: {
-		fontSize: 0.045 * SCREEN_WIDTH,
-		color: '#00C1FF',
-		textAlign: 'left',
-		fontWeight: 'normal'
-	},
-	noAccountTextStyle: {
-		color: 'white',
-		fontSize: 0.047 * SCREEN_WIDTH,
-		fontWeight: '400'
-	},
-	TextViewStyle: {
-		flex: 7,
-		alignItems: 'center'
-	}
+  containerStyle: {
+    flexDirection: 'row',
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0)',
+    margin: 10
+  },
+  SignUpButtonStyle: {
+    borderRadius: 0,
+    backgroundColor: 'rgba(0,0,0,0)'
+  },
+  SignUpContainer: {
+    flex: 3,
+    alignItems: 'stretch'
+  },
+  buttonTextStyle: {
+    fontSize: 0.045 * SCREEN_WIDTH,
+    color: '#00C1FF',
+    textAlign: 'left',
+    fontWeight: 'normal'
+  },
+  noAccountTextStyle: {
+    color: 'white',
+    fontSize: 0.047 * SCREEN_WIDTH,
+    fontWeight: '400'
+  },
+  TextViewStyle: {
+    flex: 7,
+    alignItems: 'center'
+  }
 };
 
 export default SignUpAccount;
