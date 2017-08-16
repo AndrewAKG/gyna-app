dc11ec00a428759859b64808e4ec9b50c9188968
SallyHabib <sally.hanen@student.guc.edu.eg>
2017-08-16 15:06:06 +0200

    feat : editProfile scrren frontend

---
+++ src/components/InputMoreScreen.js
@@ -0,0 +1,62 @@
+/**
+ * this component is used whenever i want to take a text field as input from user
+ */
+import React from 'react';
+import { View, Text, TextInput, Image, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
+
+
+const InputMoreScreen = ({ value, placeholder, style, Type, secure, onChangeText }) => {
+  const { containerStyle, inputStyle, imageStyle } = styles;
+
+  return (
+    <View style={[containerStyle, style]}>
+      <View style={{ flex: 1, alignItems:'flex-start' }}>
+        <TextInput
+          keyboardType={Type}
+          autoCorrect={false}
+          placeholder={placeholder}
+          placeholderTextColor='white'
+          style={inputStyle}
+          value={value}
+          secureTextEntry={secure}
+          onChangeText={onChangeText}
+       //   returnKeyType='next'
+        />
+      </View>
+    </View>
+  );
+};
+
+/**
+ * Styles used for ths component , includes the input style 
+ */
+const styles = {
+  containerStyle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 0.05 * SCREEN_HEIGHT,
+    borderWidth: 0.3,
+    borderColor: 'white',
+    backgroundColor: '#5c1634',
+    height: 0.08 * SCREEN_HEIGHT,
+    width: 0.8 * SCREEN_WIDTH,
+    marginHorizontal: 20,
+    marginBottom: 20,
+    marginTop: 5
+  },
+  inputStyle: {
+    color: 'white',
+    paddingRight: 5,
+    paddingLeft: 10,
+    fontSize: 0.04 * SCREEN_WIDTH,
+    lineHeight: 23,
+    fontWeight: "200",
+    height: 50,
+    width: 0.6 * SCREEN_WIDTH,
+  }
+}
+
+export { InputMoreScreen };
--- src/screens/EditProfileScreen.js
+++ src/screens/EditProfileScreen.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, Image } from 'react-native';
-import { BackgroundImage } from '../components';
+import { View, Text, Image, ScrollView, Dimensions } from 'react-native';
+import { BackgroundImage, InputMoreScreen, BirthdateInput } from '../components';
+import { Button } from 'react-native-elements';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 class EditProfileScreen extends Component {
-    
+
   static navigationOptions = {
     title: 'editProfile',
     headerTintColor: 'white',
@@ -13,15 +17,84 @@ class EditProfileScreen extends Component {
     headerTitle: 'edit profile',
   };
 
+  constructor(props) {
+    super(props);
+    let today = new Date();
+    this.state = {
+      date: today,
+    }
+  }
+
   render() {
+    const {
+      containerStyle,
+      buttonStyle,
+      inputStyle,
+      userNameStyle,
+      emailPhoneStyle,
+      dateStyle
+     } = styles;
     return (
-      <BackgroundImage
-       />
+      <BackgroundImage>
+        <View style={{ flex: 1 }}>
+          <ScrollView
+            contentContainerStyle={containerStyle}
+          >
+            <Text style={userNameStyle}>
+              Username
+          </Text>
+            <InputMoreScreen
+              placeholder='Username'
+            />
+            <Text style={emailPhoneStyle}>
+              E.mail
+          </Text>
+            <InputMoreScreen
+              placeholder='E.mail'
+              Type='email-address'
+            />
+            <Text style={emailPhoneStyle}>
+              Phone
+          </Text>
+            <InputMoreScreen
+              placeholder='Phone'
+              Type='phone-pad'
+            />
+            <Text style={dateStyle}>
+              Working Address
+          </Text>
+            <InputMoreScreen
+              placeholder='Working Address'
+            />
+            <Text style={dateStyle}>
+              Anniversary Date
+            </Text>
+            <BirthdateInput
+              date={this.state.date}
+              onDateChange={(date) => this.setState({ date: date })}
+            />
+            <Button
+              onPress={() => console.log('change edit profile')}
+              title="Save"
+              buttonStyle={buttonStyle}
+              color='white'
+              fontWeight='bold'
+              fontSize={0.047 * SCREEN_WIDTH}
+            />
+          </ScrollView>
+
+        </View>
+      </BackgroundImage>
     );
   }
 }
 
 const styles = {
+  containerStyle: {
+    paddingTop: 20,
+    alignItems: 'center',
+    marginTop: 10
+  },
   icon: {
     width: 24,
     height: 24,
@@ -32,6 +105,38 @@ const styles = {
     fontSize: 18,
     height: 44,
   },
+  buttonStyle: {
+    borderRadius: 0.05 * SCREEN_HEIGHT,
+    backgroundColor: '#00C1FF',
+    width: 0.8 * SCREEN_WIDTH,
+    height: 0.095 * SCREEN_HEIGHT,
+    marginHorizontal: 20,
+    marginBottom: 20,
+    marginTop: 40
+  },
+  userNameStyle: {
+    color: 'white',
+    backgroundColor: 'rgba(0,0,0,0)',
+    fontSize: 16,
+    marginRight: 230,
+    fontWeight: '500'
+  },
+  emailPhoneStyle: {
+    color: 'white',
+    backgroundColor: 'rgba(0,0,0,0)',
+    fontSize: 16,
+    marginRight: 250,
+    fontWeight: '500'
+  },
+  dateStyle: {
+    color: 'white',
+    backgroundColor: 'rgba(0,0,0,0)',
+    fontSize: 16,
+    marginRight: 180,
+    fontWeight: '500'
+  }
+
+
 };
 
 export default EditProfileScreen;
